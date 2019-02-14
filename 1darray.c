c7fc770b489d9b96642056c2a6790bbb6bf0fa62
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-14 15:35:31 +0530

    Create 1darray.c

---
+++ 1darray.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n,i,s=0,a[50];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+     for(i=0;i<n;i++)
+    {
+     s=s+a[i];
+    }
+    printf("%d",s);
+    return 0;
+}
+
